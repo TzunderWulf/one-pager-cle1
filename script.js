679539d016ae935a2fcf52d3ee1230c37fa71368
TzunderWulf <svandenbos@tzunder.nl>
2021-02-21 00:08:45 +0100

    website is now responsive to all screensizes

--- script.js
+++ script.js
@@ -2,36 +2,43 @@
 
 /* navigational buttons linked to different parts of the page */
 // information section
-    const infoButton = document.querySelector("#information");
-    const infoSection = document.querySelector("#information-section");
-    infoButton.addEventListener("click", function () {
-        infoSection.scrollIntoView({ behavior: 'smooth' });
-    })
+const infoButton = document.querySelector("#info-btn");
+const infoSection = document.querySelector("#info-sec");
+infoButton.addEventListener("click", function () {
+    infoSection.scrollIntoView({ behavior: 'smooth' });
+})
 
 // showcase section
-    const showcaseButton = document.querySelector("#showcase");
-    const showcaseSection = document.querySelector("#showcase-section");
-    showcaseButton.addEventListener("click", function () {
-        showcaseSection.scrollIntoView({ behavior: 'smooth' });
-    })
+const showcaseButton = document.querySelector("#showcase-btn");
+const showcaseSection = document.querySelector("#showcase-sec");
+showcaseButton.addEventListener("click", function () {
+    showcaseSection.scrollIntoView({ behavior: 'smooth' });
+})
 
 // our mission section
-    const missionButton = document.querySelector("#our-mission");
-    const missionSection = document.querySelector("#our-mission-section");
-    missionButton.addEventListener("click", function () {
-        missionSection.scrollIntoView({ behavior: 'smooth' });
-    })
+const missionButton = document.querySelector("#mission-btn");
+const missionSection = document.querySelector("#mission-sec");
+missionButton.addEventListener("click", function () {
+    missionSection.scrollIntoView({ behavior: 'smooth' });
+})
 
-// images section
-    const imagesButton = document.querySelector("#images");
-    const imagesSection = document.querySelector("#images-section");
-    imagesButton.addEventListener("click", function () {
-        imagesSection.scrollIntoView({ behavior: 'smooth' });
-    })
+// slideshow section
+const imagesButton = document.querySelector("#images-btn");
+const imagesSection = document.querySelector("#images-sec");
+imagesButton.addEventListener("click", function () {
+    imagesSection.scrollIntoView({ behavior: 'smooth' });
+})
 
 // our team section
-    const teamButton = document.querySelector("#our-team");
-    const teamSection = document.querySelector("#our-team-section");
-    teamButton.addEventListener("click", function () {
-        teamSection.scrollIntoView({ behavior: 'smooth' });
-    })
+const teamButton = document.querySelector("#team-btn");
+const teamSection = document.querySelector("#team-sec");
+teamButton.addEventListener("click", function () {
+    teamSection.scrollIntoView({ behavior: 'smooth' });
+})
+
+// button to return to top
+const toTopButton = document.getElementById("to-top-btn");
+toTopButton.addEventListener("click", function () {
+    document.body.scrollTop = 0; // to make it work in Safari
+    document.documentElement.scrollTop = 0; // to make it work in all other browsers
+})
